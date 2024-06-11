17c57e048fef91fd933040c8660e84f853000cbe
bobo <floppybootstompdown@gmail.com>
2024-06-11 19:17:05 -0400

    free software song

--- sources/main.c
+++ sources/main.c
@@ -12,7 +12,8 @@ void generateScale(double* scale, int length, double frequency, int octave, int 
 	scale[0] = frequency;
 
 	for(int i = 1; i <= length; i ++){
-		if(i % step == 0) { frequency *= octave; }
+		if(i % step == 0)
+			frequency *= octave;
 
 		scale[i] = frequency + (i % step * (frequency * octave - frequency) / step);
 	}
@@ -55,7 +56,6 @@ double applyEnvelope(int i, double length, double strengthInit, double a, double
 		return fmax(fmin(-1 * (s + iSeconds) / r, 1), 0);
 	}
 
-	printf("sus\n");
 	return 0;
 }
 
@@ -67,11 +67,8 @@ double applyEnvelope(int i, double length, double strengthInit, double a, double
 
 // generates a tone from a tone generator function
 void generateTone(double* samples, double length, double frequency, double amplitude, double (*toneGeneratorFunction)(int, double, double)){
-	//amplitude = 0;
 	for(int i = 0; i < length; i ++){
 		samples[i] = amplitude * toneGeneratorFunction(i, frequency, amplitude);
-		// frequencyfrequency -= 1.01;
-        // if(amplitude > 0) { amplitude -= 4*(amplitude/length); } else { amplitude = 0; }
 		
 		amplitude += applyEnvelope(
 								  i,
@@ -79,7 +76,7 @@ void generateTone(double* samples, double length, double frequency, double ampli
 								  amplitude,
 								  0.0,
 								  0.0,
-								  length/4,
+								  amplitude,
 								  0.0);
 		
 	}
@@ -110,6 +107,12 @@ struct Sequence {
 	double (*toneGeneratorFunction)(int, double, double);
 	double *scale;
 
+	// in beats per second
+	float tempo;
+
+	// 1 repeat 0 no repeat
+	int repeat;
+	
 	int length;
 	// tones[][0] = pitch
 	// tones[][1] = rhythm (in fractions of a second: seconds / tones[][1])
@@ -120,21 +123,23 @@ struct Sequence {
 // writes a sequence to a sample buffer
 void writeSequence(double* sample, double length, struct Sequence* seq){
 	int bufferPos = 0;
-	for(int i = 0; i < seq->length; i ++){
+	int nextNoteSpace = 0;
+	for(int i = 0; bufferPos < length * SAMPLE_RATE - (SAMPLE_RATE / (seq->tempo * seq->tones[1][i])); i = (i+1)%seq->length){
+		nextNoteSpace = SAMPLE_RATE / (seq->tempo * seq->tones[1][i]);
 		generateTone(
 					 &sample[bufferPos],
-					 SAMPLE_RATE/seq->tones[1][i],
+					 nextNoteSpace,
 					 seq->scale[seq->tones[0][i]],
 					 (double)seq->tones[2][i]/100,
 					 seq->toneGeneratorFunction
 					 );
 
-		bufferPos += (SAMPLE_RATE/seq->tones[1][i]);
+		bufferPos += nextNoteSpace;
 	}
 }
 
 int main(int argv, char* argc[]){
-	double duration = 3;
+	double duration = 13.25;
 	double frequency = 110;
 	double amplitude = 0.8;
 	double* samples = (double*)malloc(sizeof(double) * duration * SAMPLE_RATE);
@@ -143,17 +148,17 @@ int main(int argv, char* argc[]){
 
 	char* outputFile = "./data/prunes.pcm";
 
-	int ton[12] = { 12, 10, 8 , 6 , 8 , 10, 8 , 6 , 4 , 6 , 8 , 6  };
-	int rhy[12] = { 4 , 8 , 4 , 4 , 4 , 8 , 4 , 4 , 3 , 3 , 3 , 8  };
-	int amp[12] = { 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80 };
+	int ton[18] = { 17, 15, 14, 12, 14, 15, 14, 10, 10, 12, 15, 14, 17, 12, 12, 17, 15, 14 };
+	int rhy[18] = { 4 , 8 , 4 , 4 , 4 , 8 , 4 , 4 , 3 , 3 , 3 , 3 , 4 , 3 , 3 , 4 , 8 , 2  };
+	int amp[18] = { 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80 };
 
 	struct Sequence seq = {
 		&generateSine,
 		s,
-		12,
-		{ton,
-		 rhy,
-		 amp}
+		0.75,
+		1,
+		18,
+		{ton, rhy, amp}
 	};
 
 	// write sequence
